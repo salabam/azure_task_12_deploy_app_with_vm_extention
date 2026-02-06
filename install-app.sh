daef838f84c7a3cb44ba76004902277afb345139
Pavlus Chaban <pavluschaban@MacBook-Pro-Olena.local>
2026-02-06 03:51:07 +0200

    Solution

--- install-app.sh
+++ install-app.sh
@@ -9,11 +9,12 @@
 # when running the script as VM extention.  
 apt-get update -yq
 apt-get install python3-pip -yq
+apt-get install git -yq
 
 # Create a directory for the app and download the files. 
 mkdir /app 
 # make sure to uncomment the line bellow and update the link with your GitHub username
-# git clone https://github.com/<your-gh-username>/azure_task_12_deploy_app_with_vm_extention.git
+git clone https://github.com/salabam/azure_task_12_deploy_app_with_vm_extention.git
 cp -r azure_task_12_deploy_app_with_vm_extention/app/* /app
 
 # create a service for the app via systemctl and start the app
